89c5bc750afd778e614275a3c951f15fadd580d4
Per Åkerström <akerstrom.perry@gmail.com>
2019-07-29 18:02:44 +0200

    Initial code commit
    Base functionality, lists, multiple windows, etc

---
+++ PostIts/PostItForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PostIts
+{
+    partial class PostItForm 
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.NewWindowButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox.ForeColor = System.Drawing.Color.Ivory;
+            this.TextBox.Location = new System.Drawing.Point(12, 36);
+            this.TextBox.MinimumSize = new System.Drawing.Size(10, 10);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(260, 213);
+            this.TextBox.TabIndex = 2;
+            this.TextBox.Text = "";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.BackColor = System.Drawing.Color.DarkGray;
+            this.ExitButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ExitButton.Location = new System.Drawing.Point(250, 5);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(25, 25);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.UseVisualStyleBackColor = false;
+            // 
+            // NewWindowButton
+            // 
+            this.NewWindowButton.BackColor = System.Drawing.Color.DarkGray;
+            this.NewWindowButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.NewWindowButton.Location = new System.Drawing.Point(10, 5);
+            this.NewWindowButton.Name = "NewWindowButton";
+            this.NewWindowButton.Size = new System.Drawing.Size(25, 25);
+            this.NewWindowButton.TabIndex = 3;
+            this.NewWindowButton.UseVisualStyleBackColor = false;
+            // 
+            // PostItForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.NewWindowButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.TextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(80, 50);
+            this.Name = "PostItForm";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button NewWindowButton;
+    }
+}
+
---
+++ PostIts/PostItForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PostIts
+{
+    public partial class PostItForm : Form
+    {
+        private const int cGrip = 16;      // Grip size
+        private const int cCaption = 32;   // Caption bar height;
+        private int indentLevel = 0;
+        private const int IndentStep = 20;
+        private bool strike = false;
+
+        public PostItForm()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.FixedWidth, true);
+
+            ExitButton.Click += Exit;
+            NewWindowButton.Click += NewWindow;
+
+            TextBox.KeyDown += TextBoxKeyDown;
+
+            TextBox.AcceptsTab = true;
+            TextBox.DetectUrls = true;
+            TextBox.ScrollBars = RichTextBoxScrollBars.Vertical;
+            TextBox.Select();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rc = new Rectangle(ClientSize.Width - cGrip, ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, BackColor, rc);
+            rc = new Rectangle(0, 0, ClientSize.Width, cCaption);
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(64,64,64)), rc);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void NewWindow(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() => Application.Run(new PostItForm()));
+            thread.Start();
+        }
+
+        private void TextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.K && e.Control)
+            {
+                TextBox.SelectionFont = new Font(TextBox.SelectionFont, FontStyle.Strikeout);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                strike = true;
+                return;
+            }
+            {
+                strike = false;
+                TextBox.SelectionFont = new Font(TextBox.SelectionFont, FontStyle.Regular);
+            }
+
+            if (TextBox.SelectionBullet)
+            {
+                if (e.KeyCode == Keys.Tab)
+                    IndentList(e);
+                else if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Back)
+                    BreakList(e);
+            }
+            else
+            {
+                if (e.Control && e.KeyCode == Keys.OemQuestion)
+                    CreateList(e);
+            }
+        }
+        private void IndentList(KeyEventArgs e)
+        {
+            //TODO: check for selection
+            int lineIndex = TextBox.GetLineFromCharIndex(TextBox.SelectionStart);
+            string line = (lineIndex < 0 || lineIndex >= TextBox.Lines.Length) ? "" : TextBox.Lines[lineIndex];
+
+            bool fullSelect = TextBox.SelectionLength > 0 && TextBox.SelectionLength == line.Length;
+            line = line.Trim();
+            if (line.Length != 0 && !fullSelect)
+                return;
+            
+            indentLevel = Math.Max(indentLevel + (e.Shift ? -1 : 1), 0);            
+            TextBox.SelectionIndent = indentLevel * IndentStep;
+            e.SuppressKeyPress = true;
+            e.Handled = true;       
+        }
+        private void BreakList(KeyEventArgs e)
+        {
+            int lineIndex = TextBox.GetLineFromCharIndex(TextBox.SelectionStart);
+            if (lineIndex < 0 || lineIndex >= TextBox.Lines.Length)
+                return;
+
+            if (TextBox.Lines[lineIndex].Length > 0)
+                return;
+
+            TextBox.SelectionBullet = false;
+            indentLevel = 0;
+            TextBox.SelectionIndent = indentLevel * IndentStep;
+            e.SuppressKeyPress = true;
+            e.Handled = true;
+        }
+        private void CreateList(KeyEventArgs e)
+        {
+            TextBox.SelectionBullet = true;
+            e.SuppressKeyPress = true;
+            e.Handled = true;
+        }
+
+    }
+}
